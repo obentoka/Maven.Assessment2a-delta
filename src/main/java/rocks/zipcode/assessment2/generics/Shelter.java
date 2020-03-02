0f4bfe61940cb099ddd888c2d42f0267479f6d49
vle <obentoka@gmail.com>
2020-03-02 12:28:14 -0500

    Generics done

--- src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
+++ src/main/java/rocks/zipcode/assessment2/generics/Shelter.java
@@ -1,6 +1,13 @@
 package rocks.zipcode.assessment2.generics;
 
 
+import rocks.zipcode.assessment2.generics.ageable.Ageable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
 /**
  * Shelter is a generic class that holds Objects that extends `Ageable`.
  * For example, if a Person and a Dog extends Ageable, then I can declare the following:
@@ -8,33 +15,52 @@ package rocks.zipcode.assessment2.generics;
  * Shelter<Person> farmHouse = new Shelter<Person>();
  * Shelter<Dog> dogHouse = new Shelter<Dog>();
  */
-public class Shelter<_> {
+
+//CHANGED ALL THE PARAMETER TO GENERICS >>????><<
+public class Shelter<T extends Ageable> implements Iterable<T>{
+
+    List<T> shell;
+
     public Shelter() {
-        throw new NullPointerException();
+        shell = new ArrayList<>();
     }
 
     /**
      * @return the number of item in the shelter
      */
     public int size() {
-        return -1;
+        if(shell != null)
+            return shell.size();
+        return (Integer) null;
     }
 
-    public void add(Object object) {
+    public void add(T object) {
+        shell.add(object);
     }
 
-    public Boolean contains(Object object) {
-        return null;
+    public Boolean contains(T object) {
+        return shell.contains(object);
     }
 
-    public void remove(Object object) {
+    public void remove(T object) {
+        if(shell != null)
+            shell.remove(object);
     }
 
     public Object get(Integer index) {
+        if(shell != null)
+            return shell.get(index);
         return null;
     }
 
-    public Integer getIndexOf(Object ageable) {
-        return 0;
+    public Integer getIndexOf(T ageable) {
+        if(shell != null)
+            return shell.indexOf(ageable);
+        return null;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
     }
 }
