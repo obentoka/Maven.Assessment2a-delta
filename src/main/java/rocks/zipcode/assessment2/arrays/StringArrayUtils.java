018b5b3dcf0c67cfad103a7ff13adddd995bbf49
vle <obentoka@gmail.com>
2020-03-02 09:47:56 -0500

    AddItem to inventory done

--- src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
+++ src/main/java/rocks/zipcode/assessment2/arrays/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.assessment2.arrays;
 
+import java.util.logging.Logger;
+
 /**
  * @author leon on 28/11/2018.
  */
@@ -10,7 +12,22 @@ public class StringArrayUtils {
      * @param endingIndex - ending index of array
      * @return an array with all elements between `startingIndex` and `endingIndex`
      */
+    private static final Logger LOGGER =
+            Logger.getLogger((StringArrayUtils.class.getName()));
+
     public static String[] getSubArray(String[] arrayToBeSpliced, int startingIndex, int endingIndex) {
+        try {
+            if ((startingIndex >= 0 && startingIndex < arrayToBeSpliced.length) &&
+                    (endingIndex < arrayToBeSpliced.length && endingIndex > 1)) {
+                String[] retString = new String[endingIndex - startingIndex];
+                for (int i = 0; startingIndex < endingIndex; i++, startingIndex++) {
+                    retString[i] = arrayToBeSpliced[startingIndex];
+                }
+                return retString;
+            }
+        }catch (IndexOutOfBoundsException e){
+            LOGGER.info(""+e);
+        }
         return null;
     }
 
@@ -21,6 +38,15 @@ public class StringArrayUtils {
      * @return an array all elements between after `startingIndex`
      */
     public static String[] getEndingArray(String[] arrayToBeSpliced, int startingIndex) {
-        return null;
+        if(startingIndex < arrayToBeSpliced.length) {
+            String[] retString = new String[arrayToBeSpliced.length - startingIndex];
+            for (int i = startingIndex; i < arrayToBeSpliced.length; i++) {
+                retString[i] = arrayToBeSpliced[i];
+            }
+            return retString;
+        }else {
+            String[] retString = new String[0];
+            return retString;
+        }
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,6 +1,8 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Use a map to keep track of inventory in a store
@@ -9,29 +11,36 @@ public class Inventory {
     /**
      * @param strings list of strings to add / remove / fetch from
      */
+    List<String> things;
+    private static final Logger LOGGER =
+            Logger.getLogger((Inventory.class.getName()));
     public Inventory(List<String> strings) {
-
+        things = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+        things = new ArrayList<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        things.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        try{
+            things.remove(item);
+        }catch (Exception e){
+            LOGGER.info(""+e);
+        }
     }
 
     /**
@@ -39,6 +48,16 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        if(things.contains(item)){
+            int count = 0;
+            for (String i : things){
+                if(i.equalsIgnoreCase(item)) {
+                    count++;
+                }
+            }
+            return count;
+        }else {
+            return 0;
+        }
     }
 }
